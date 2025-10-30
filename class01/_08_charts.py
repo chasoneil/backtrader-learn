ecb68dd77e53c51b4eb37274c04757fecd70ac40
chasoneil <chason001@126.com>
2025-10-30 18:01:46 +0800

    review

--- class01/_08_charts.py
+++ class01/_08_charts.py
@@ -22,14 +22,19 @@ class TestStrategy(bt.Strategy):
         self.sma = bt.indicators.SimpleMovingAverage(
             self.datas[0], period=self.params.maperiod)
 
-        # Indicators for the plotting show
+        # 创建一个周期为25的指数移动平均线（EMA）指标实例，并将其添加到策略中。EMA用于反映价格变化的加权平均，更加强调最近的价格数据。
         bt.indicators.ExponentialMovingAverage(self.datas[0], period=25)
-        bt.indicators.WeightedMovingAverage(self.datas[0], period=25,
-                                            subplot=True)
+        # 创建一个周期为25的加权移动平均线（WMA）指标实例，并将其添加到策略中。subplot=True表示这个指标将在一个新的子图中绘制。WMA也是一种加权平均线，但是它对所有数据点加权，权重随着数据点距离当前时间的远近递减。
+        bt.indicators.WeightedMovingAverage(self.datas[0], period=25, subplot=True)
+        # 创建一个慢速随机指标（Stochastic Slow）实例，并将其添加到策略中。这个指标用于衡量市场是否处于超买或超卖状态，以及价格的变化动量。
         bt.indicators.StochasticSlow(self.datas[0])
+        # 创建一个MACD直方图（MACDHisto）指标实例，并将其添加到策略中。MACD直方图用于显示MACD线和信号线之间的差异，当直方图为正时，表示市场处于买入状态；当直方图为负时，表示市场处于卖出状态
         bt.indicators.MACDHisto(self.datas[0])
+        # 创建一个相对强弱指数（RSI）指标实例，并将其赋值给变量rsi。RSI用于衡量市场的超买和超卖条件，通常RSI大于70表示超买，小于30表示超卖。
         rsi = bt.indicators.RSI(self.datas[0])
+        # 创建一个周期为10的RSI的平滑移动平均线（Smoothed Moving Average of RSI）实例，并将其添加到策略中。这个平滑的RSI可以用来过滤掉RSI的短期波动，提供更稳定的买卖信号。
         bt.indicators.SmoothedMovingAverage(rsi, period=10)
+        # 创建一个平均真实波幅（ATR）指标实例，并将其添加到策略中。plot=False表示这个指标不会在图表中绘制出来。ATR用于衡量市场的波动性，计算方法是取过去周期内最高价、最低价和前一天收盘价之间的最大值，然后计算14周期的简单移动平均。
         bt.indicators.ATR(self.datas[0], plot=False)
 
     def notify_order(self, order):
